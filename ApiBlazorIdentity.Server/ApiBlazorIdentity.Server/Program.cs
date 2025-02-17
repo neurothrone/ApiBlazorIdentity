c1b6e5fda05429f4be4d59603de03fc83ae0ddf5
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-02-17 05:09:10 +0100

    remove commented-out AuthenticationStateProvider registration in Program.cs

--- ApiBlazorIdentity.Server/ApiBlazorIdentity.Server/Program.cs
+++ ApiBlazorIdentity.Server/ApiBlazorIdentity.Server/Program.cs
@@ -19,7 +19,6 @@ builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddScoped<IdentityUserAccessor>();
 builder.Services.AddScoped<IdentityRedirectManager>();
 builder.Services.AddScoped<AuthenticationStateProvider, PersistingRevalidatingAuthenticationStateProvider>();
-// builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
 
 builder.Services.AddAuthentication(options =>
     {
